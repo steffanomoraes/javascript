33589a399ff0f5a8aa02161fdd74e5ce58f2d7a8
Augusto Steffano Moraes Pires <steffanomoraes1@gmail.com>
2024-02-19 09:13:18 -0300

    exercicio 15

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,16 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('inas')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano)
+    {
+        window.alert('Verifique os dados e tente novamente!')
+    }
+    else {
+        fsex = document.getElementsByName('sex')
+        var idade = ano - fano.value 
+        res.innerHTML = `Idade calculada: ${idade}`
+    }
+}
